c68a0cc4b7831dedf6694202eda815d5c39c4193
Alfredo <segalaalfredo16@gmail.com>
2025-04-03 20:55:57 +0200

    Add relationship model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_01_123742) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_03_143813) do
   create_table "books", force: :cascade do |t|
     t.string "isbn"
     t.datetime "created_at", null: false
@@ -50,6 +50,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_01_123742) do
     t.datetime "updated_at", null: false
     t.string "creator"
     t.string "book"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "sessions", force: :cascade do |t|
