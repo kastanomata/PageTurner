e8e0c8e1965e436b46545cd46b233dfce4842f57
Nino2109 <alessandrotemperini21@gmail.com>
2025-04-18 18:21:25 +0200

    Little changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+    #navbar search form
+    def render_search_form
+        content_tag :div do
+          form_tag(search_path, method: :get) do
+            safe_join([
+              text_field_tag(:query, params[:query], placeholder: "Cerca..."),
+              submit_tag("Cerca")
+            ])
+          end
+        end
+      end
+
 end
