dcd48134f95669adc243295ae8bd44b9b55a935f
Nino2109 <alessandrotemperini21@gmail.com>
2025-04-18 19:18:40 +0200

    little changes showpost and homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
 
-    #navbar search form
-    def render_search_form
-        content_tag :div do
-          form_tag(search_path, method: :get) do
-            safe_join([
-              text_field_tag(:query, params[:query], placeholder: "Cerca..."),
-              submit_tag("Cerca")
-            ])
-          end
-        end
+  #navbar search form
+  def render_search_form
+    content_tag :div, class: "search-form-wrapper" do
+      form_tag(search_path, method: :get, class: "search-form") do
+        safe_join([
+          text_field_tag(:query, params[:query], placeholder: "Cerca...", class: "search-input"),
+          submit_tag("Cerca", class: "search-button")
+        ])
       end
+    end
+  end
 
 end
