cd4db6c0f35964465059e03e15df13e3598e0265
Nino2109 <alessandrotemperini21@gmail.com>
2025-05-13 13:02:39 +0200

    show post; show club with end error to fix

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -24,9 +24,9 @@ module PostsHelper
     pre_like = post.likes.find { |like| like.user_id == Current.user&.id }
 
     if pre_like
-      button_to "Unlike", post_like_path(post, pre_like), method: :delete
+      button_to "Unlike", post_like_path(post, pre_like), method: :delete, class: "btn btn--primary btn--sm"
     else
-      button_to "Like", post_likes_path(post), method: :post
+      button_to "Like", post_likes_path(post), method: :post, class:"btn btn--primary btn--sm"
     end
   end
 
