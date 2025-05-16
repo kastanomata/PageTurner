50e9bcf16115edf84fc0d3c431619c675f23fe6d
antonyo.turco <antonyo.turco@gmail.com>
2025-05-16 13:56:42 +0200

    Add comments and likes seeding

--- lib/initialize_utility.rb
+++ lib/initialize_utility.rb
@@ -1,25 +1,61 @@
 include LoggingUtility
 module InitializeUtility
   def initialize_user(user)
-    # Create default bookshelves for the user
-    read_bookshelf  = user.create_bookshelf(name: "#{user.nickname}'s Read Books", special: :true)
-    liked_bookshelf = user.create_bookshelf(name: "#{user.nickname}'s Liked Books", special: :true)
+    # Load sample comments
+    comments_path = Rails.root.join("db", "seeds", "comments.json")
+    comments_data = JSON.parse(File.read(comments_path))
 
-    # Populate the special bookshelves with books from the user's posts
-    user.posts.each do |post|
-      read_bookshelf.add_book(post.book)
-      liked_bookshelf.add_book(post.book) if rand < 1.0 / 3
-    end
+    # Create default bookshelves
+    read_bookshelf = user.create_bookshelf(name: "#{user.nickname}'s Read Books", special: true)
+    liked_bookshelf = user.create_bookshelf(name: "#{user.nickname}'s Liked Books", special: true)
+
     # Create relationships and memberships
-    random_follows = User.ids.sample(3)
-    random_follows.each do |id|
-      if user.id != id
-        Relationship.create_or_find_by!(follower_id: user.id, followed_id: id)
+    random_follows = User.where.not(id: user.id).sample(3)
+    random_follows.each do |followed_user|
+      user.follow(followed_user)
+
+      # Like and comment on some of their posts
+      followed_user.posts.each do |post|
+        # 75% chance to like the post
+        if rand < 0.75
+          user.likes.create(post: post)
+        end
+
+        # 50% chance to comment on the post
+        if rand < 0.5
+          comment_content = comments_data.sample
+          post.comments.create(
+            author_id: user.id,
+            text: comment_content,
+            created_at: rand(1..30).days.ago # Randomize comment timing
+          )
+        end
       end
     end
-    random_memberships = Club.ids.sample(1)
-    random_memberships.each do |id|
-      Membership.create_or_find_by!(follower_id: user.id, club_id: id)
+
+    # Join random clubs
+    Club.all.sample(1).each do |club|
+      Membership.create_or_find_by!(follower_id: user.id, club_id: club.id)
+
+      # Like and comment on some club posts
+      club.posts.sample(3).each do |post|
+        if rand < 0.5 # 50% chance to interact with club posts
+          user.likes.create(post: post) if rand < 0.75
+          if rand < 0.5
+            post.comments.create(
+              author_id: user.id,
+              text: comments_data.sample,
+              created_at: rand(1..30).days.ago
+            )
+          end
+        end
+      end
+    end
+
+    # Populate bookshelves with books from posts
+    user.posts.each do |post|
+      read_bookshelf.bookshelf_contains.create(book: post.book)
+      liked_bookshelf.bookshelf_contains.create(book: post.book) if rand < 1.0/3
     end
   end
 
@@ -36,8 +72,6 @@ module InitializeUtility
     book_details = BookApiService.fetch_book_details(isbn)
     book_details[:isbn] = isbn
     return warning("Book not found: #{isbn}") unless book_details
-    puts "Author OpenLibrary ID: #{book_details[:author_openlibrary_id].inspect}"
-    puts "Initialize author result: #{initialize_author(book_details[:author_openlibrary_id]).inspect}"
 
     author = if book_details[:author_openlibrary_id].present?
               Author.find_by(openlibrary_id: book_details[:author_openlibrary_id]) ||
