a43eb640d5238a77c5c3eb8ecda39c68e2d38c74
Alfredo <segalaalfredo@gmail.com>
2025-05-19 14:44:18 +0200

    Adding test controller post

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -5,6 +5,7 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     @post = posts(:one)
     @other_post = posts(:two)
     @user = users(:one)
+    @user3 = users(:three)
   end
 
   test "should get index" do
@@ -37,6 +38,36 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to post_path(Post.last)
+  end
+
+  test "should report post" do
+    post session_path, params: { email_address: @user3.email_address, password: "password" }
+    assert_equal @user3.id, session[:user_id]
+    assert_difference("Report.count") do
+      post reports_path, params: { report: { reporter_id: @user3.id, reported_id: @post.id, reported_type: "Post" } }
+    end
+
+    assert_response :success
+  end
+
+  test "like post" do
+    post session_path, params: { email_address: @user3.email_address, password: "password" }
+    assert_equal @user3.id, session[:user_id]
+    assert_difference("Like.count") do
+      post post_likes_path(@post), params: { like: { user_id: @user3.id, post_id: @post.id } }
+    end
+
+    assert_redirected_to post_path(@post)
+  end
+
+  test "comment post" do
+    post session_path, params: { email_address: @user3.email_address, password: "password" }
+    assert_equal @user3.id, session[:user_id]
+    assert_difference("Comment.count") do
+      post post_comments_path(@post), params: { comment: { author_id: @user3.id, post_id: @post.id, text: "Che mina" } }
+    end
+
+    assert_redirected_to post_path(@post)
   end
 
   test "should show post" do
